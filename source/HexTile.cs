78996569991d452329e73d334ed92a03bd5e8a7a
nextom1 <nextom@outlook.com>
2021-05-24 16:26:27 +0200

    Change to cube coordinates (#36)

--- source/HexTile.cs
+++ source/HexTile.cs
@@ -3,7 +3,13 @@ using System;
 
 public class HexTile : Sprite
 {
-    public record HexIndex(int Row, int Column);
+    /// <summary>
+    /// Cube coordinates based index.
+    /// </summary>
+    /// <param name="X">Left to right</param>
+    /// <param name="Y">Bottom right to top left</param>
+    /// <param name="Z">Top right to bottom left</param>
+    public record HexIndex(int X, int Y, int Z);
 
     public HexIndex Index { get; init; }
 }
