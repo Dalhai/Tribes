11f2c8f339b7aa8711b7cbf6f2ee1f2b031a0fad
Marcel Marti <marcel.marti.89@gmail.com>
2021-06-12 18:39:23 +0200

    Fix hex conversions for hex to world coordinates (#36)

--- source/hex/HexConversions.cs
+++ source/hex/HexConversions.cs
@@ -20,28 +20,41 @@ namespace TribesOfDust.Hex
             var q = position.x * 2.0f / (3.0f * size);
             var r = position.x * -1.0f/ (3.0f * size) + position.y * 1.0f / (Mathf.Sqrt(3.0f) * size);
 
-            return HexRound(new AxialCoordinate<float>(q, r).ToCubeCoordinate());
+            return HexRound(CubeCoordinate.FromQR(q, r));
         }
 
-        private static AxialCoordinate<int> HexRound(CubeCoordinate<float> cubeCoordinate)
+        private static AxialCoordinate<int> HexRound(CubeCoordinate<float> coordinate)
         {
-            var roundedCubeCoordinate = cubeCoordinate.Round();
-            var dx = Mathf.Abs(roundedCubeCoordinate.X - cubeCoordinate.X);
-            var dy = Mathf.Abs(roundedCubeCoordinate.Y - cubeCoordinate.Y);
-            var dz = Mathf.Abs(roundedCubeCoordinate.Z - cubeCoordinate.Z);
+            var rounded = coordinate.Round();
+            var delta = new CubeCoordinate<float>(
+                Mathf.Abs(rounded.X - coordinate.X),
+                Mathf.Abs(rounded.Y - coordinate.Y),
+                Mathf.Abs(rounded.Z - coordinate.Z)
+            );
 
-            var (rx, ry, rz) = roundedCubeCoordinate;
+            // Find the component with the largest offset and reset it to what is needed by the other components.
+            // This is necessary because for cube coordinates, x + y + z must be fullfilled.
 
-            if (dx > dy && dx > dz)
+            int rx = rounded.X, ry = rounded.Y, rz = rounded.Z;
+            // For our floating point cube coordinate this has to be the case. For our rounded
+            // floating point cube coordinate, this might however not be the case anymore.
+            // Readjusting the component that is wrong "the most", will fix this issue and give
+            // us the correct coordinate again.
+
+            if (delta.X >= delta.Y && delta.X >= delta.Z)
             {
-                rx = -ry - rz;
+                rx = -(ry + rz);
             }
-            else if (dy <= dz)
+            else if (delta.Y >= delta.X && delta.Y >= delta.Z)
             {
-                rz = -rx - ry;
+                ry = -(rx + rz);
+            }
+            else if (delta.Z >= delta.X && delta.Z >= delta.Y)
+            {
+                rz = -(rx + ry);
             }
 
-            return new(rx, rz);
+            return new CubeCoordinate<int>(rx, ry, rz).ToAxialCoordinate();
         }
     }
 }
