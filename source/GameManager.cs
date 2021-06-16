bc69f5f6bf760e89011aeb84229591365289c550
nextom1 <nextom@outlook.com>
2021-06-16 22:29:55 +0200

    Remove debug code (#38)

--- source/GameManager.cs
+++ source/GameManager.cs
@@ -1,57 +1,30 @@
 using Godot;
-using System;
 using System.Collections.Generic;
 using TribesOfDust.Hex;
 
 namespace System.Runtime.CompilerServices
 {
-    internal class IsExternalInit {};
+    internal class IsExternalInit
+    {
+    };
 }
 
 namespace TribesOfDust
 {
-
     public class GameManager : Node2D
     {
-        private static readonly Lazy<Texture> TileTexture = new(() => GD.Load<Texture>("res://assets/textures/tile_mountain_1.png"));
-        private static readonly Lazy<Texture> DebugTexture = new(() => GD.Load<Texture>("res://assets/textures/debug/circle.png"));
-
-        private static readonly Lazy<Sprite> DebugSpriteTL = new(() => new Sprite() { Centered = true, Texture = DebugTexture.Value, ZIndex = 10 });
-        private static readonly Lazy<Sprite> DebugSpriteTR = new(() => new Sprite() { Centered = true, Texture = DebugTexture.Value, ZIndex = 10 });
-        private static readonly Lazy<Sprite> DebugSpriteBL = new(() => new Sprite() { Centered = true, Texture = DebugTexture.Value, ZIndex = 10 });
-        private static readonly Lazy<Sprite> DebugSpriteBR = new(() => new Sprite() { Centered = true, Texture = DebugTexture.Value, ZIndex = 10 });
-
-        private static readonly Lazy<Sprite> DebugSpriteMP = new(() => new Sprite() { Centered = true, Texture = DebugTexture.Value, ZIndex = 10 });
-
-        private readonly Dictionary<AxialCoordinate<int>, HexTile> _tiles = new ();
+        private readonly Dictionary<AxialCoordinate<int>, HexTile> _tiles = new();
         private HexTile? _activeTile;
-
-        private float _size;
 
         public override void _Ready()
         {
-            AddChild(DebugSpriteTL.Value);
-            AddChild(DebugSpriteTR.Value);
-            AddChild(DebugSpriteBL.Value);
-            AddChild(DebugSpriteBR.Value);
-            AddChild(DebugSpriteMP.Value);
-
-            _size = TileTexture.Value.GetWidth() / 2.0f;
-
             for (int x = 0; x < 10; ++x)
             {
                 for (int z = 0; z < 10; ++z)
                 {
                     var axialCoordinates = new AxialCoordinate<int>(x, z);
-                    var position = HexConversions.HexToWorld(axialCoordinates, _size);
-
-                    var tile = new HexTile(axialCoordinates,TileType.Open,new())
-                    {
-                        Texture = TileTexture.Value,
-                        Centered = true,
-                        Position = position
-                    };
-
+                    var tile = new HexTile(axialCoordinates, TileType.Open);
+                    
                     AddChild(tile);
                     _tiles.Add(tile.Coordinates, tile);
                 }
@@ -63,19 +36,7 @@ namespace TribesOfDust
             if (inputEvent is InputEventMouseMotion)
             {
                 var world = GetGlobalMousePosition();
-                var hex = HexConversions.WorldToHex(world, _size);
-                var debug = HexConversions.HexToWorld(hex, _size);
-
-                Vector2 topLeft = debug + Vector2.Left * _size + Vector2.Up * _size;
-                Vector2 topRight = debug + Vector2.Right * _size + Vector2.Up * _size;
-                Vector2 bottomLeft = debug + Vector2.Left * _size + Vector2.Down * _size;
-                Vector2 bottomRight = debug + Vector2.Right * _size + Vector2.Down * _size;
-
-                DebugSpriteTL.Value.Position = topLeft;
-                DebugSpriteTR.Value.Position = topRight;
-                DebugSpriteBL.Value.Position = bottomLeft;
-                DebugSpriteBR.Value.Position = bottomRight;
-                DebugSpriteMP.Value.Position = world;
+                var hex = HexConversions.WorldToHex(world, HexTile.Size);
 
                 if (_activeTile?.Coordinates != hex && _tiles.TryGetValue(hex, out HexTile tile))
                 {
--- source/hex/HexTile.cs
+++ source/hex/HexTile.cs
@@ -6,15 +6,35 @@ namespace TribesOfDust.Hex
 {
     public class HexTile : Sprite
     {
-        public HexTile(AxialCoordinate<int> coordinates, TileType type, List<TileEffect> effects)
+        private static readonly Lazy<Texture> MountainTexture = new(() => GD.Load<Texture>("res://assets/textures/tile_mountain_1.png"));
+        private static readonly Lazy<Texture> DefaultTexture = new(() => GD.Load<Texture>("res://assets/textures/tile_mountain_1.png"));
+        
+        public static float Size => DefaultTexture.Value.GetWidth() / 2.0f;
+        
+        public HexTile(AxialCoordinate<int> coordinates, TileType type)
         {
+            
             Coordinates = coordinates;
             Type = type;
-            Effects = effects;
+            Effects = new();
+            Centered = true;
+            Position = HexConversions.HexToWorld(coordinates, Size);
+
+            switch (Type)
+            {
+                case TileType.Rocks:
+                    Texture = MountainTexture.Value;
+                    break;
+                default:
+                    Texture = DefaultTexture.Value;
+                    Modulate = Colors.Fuchsia;
+                    break;
+            }
         }
 
         public AxialCoordinate<int> Coordinates { get; }
         public TileType Type { get; }
         public List<TileEffect> Effects { get; }
     }
+    
 }
