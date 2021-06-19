11ed5b391f169da3b76fddffccbb8739347aaefc
nextom1 <nextom@outlook.com>
2021-06-19 12:45:14 +0200

    Add initial map template class (#40)

---
+++ source/map/MapTemplate.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using TribesOfDust.Hex;
+
+namespace TribesOfDust.Map
+{
+    public class MapTemplate
+    {
+        private Dictionary<AxialCoordinate<int>, TileType> _tiles = new();
+        private Dictionary<TileType, int> _tilePool = new();
+        private List<AxialCoordinate<int>> _startCoordinates = new();
+        private List<AxialCoordinate<int>> _fountainCoordinates = new();
+    }
+}
