4133a9a9ae5857d43baffa10b21a1bb1c0376d49
Marcel Marti <marcel.marti.89@gmail.com>
2021-07-10 01:24:12 +0200

    Add better exception handling (#39)

--- source/hex/TileAsset.cs
+++ source/hex/TileAsset.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using Godot;
+using TribesOfDust.Utils;
 
 namespace TribesOfDust.Hex
 {
@@ -13,25 +14,36 @@ namespace TribesOfDust.Hex
         /// <summary>
         /// Loads all tile assets in the path specified by <see cref="TileAsset.Path"/>.
         /// </summary>
+        /// <exception cref="GodotException">Thrown when the default directory can't be opened.</exception>
+        /// <exception cref="GodotException">Thrown when the default directory can't be iterated.</exception>
         /// <returns>The loaded tile assets, if available, or an empty list otherwise.</returns>
         public static List<TileAsset> LoadAll()
         {
             var dir = new Directory();
-            var err = dir.Open(Path);
 
+            // Open our default directory and abort immediately if we get
+            // an error.
+
+            var err = dir.Open(Path);
             if (err != Error.Ok)
             {
-                return new();
+                throw new GodotException(err);
             }
+
+            // Start directory iteration and abort immediately if we get
+            // an error.
 
             err = dir.ListDirBegin(skipNavigational: true);
             if (err != Error.Ok)
             {
-                return new();
+                throw new GodotException(err);
             }
 
+            // Iterate through the directory entries and try to load the
+            // corresponding tile assets.
+
             var results = new List<TileAsset>();
-            string name = dir.GetNext();
+            var name = dir.GetNext();
             while (!string.IsNullOrEmpty(name))
             {
                 results.Add(GD.Load<TileAsset>($"{Path}/{name}"));
