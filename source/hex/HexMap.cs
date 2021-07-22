ec7b3eb4cc70121111a367aca8e54518b8bc5a88
Marcel Marti <mm@m-co.ch>
2021-07-22 23:08:06 +0200

    Update GameManager to new HexMap class (#51)

--- source/hex/HexMap.cs
+++ source/hex/HexMap.cs
@@ -3,34 +3,38 @@ using System.Linq;
 using System.Collections;
 using System.Collections.Generic;
 
+using Godot;
+
 
 namespace TribesOfDust.Hex
 {
-    public class HexMap : IEnumerable<HexTile>
+    public class HexMap : Node2D, IEnumerable<HexTile>
     {
+        /// <summary>
+        /// Initializes an empty <see cref="HexMap"/>.
+        /// </summary>
         public HexMap()
         {
-            tiles = new();
         }
 
+        /// <summary>
+        /// Initializes a new <see cref="HexMap"/>.
+        /// </summary>
+        ///
+        /// <exception cref="ArgumentException">
+        /// Thrown when there are multiple tiles with the same coordinates in the initial tile list.
+        /// </exception>
+        ///
+        /// <param name="tiles">The initial tiles of the map.</param>
         public HexMap(IEnumerable<HexTile> tiles)
         {
+            // Build an index of tile coordinates to tiles.
+            // Automatically performs validity checks for successively added tiles.
 
-            // Verify the tiles are valid
-
-            bool hasDuplicateCoordinates = tiles
-                .GroupBy(tile => tile.Coordinates)
-                .Select(group => group.Count())
-                .Any(count => count > 1);
-
-            if (hasDuplicateCoordinates)
+            foreach (var tile in tiles)
             {
-                throw new ArgumentException("Duplicate tile coordinates in tile map.", "tiles");
+                AddTile(tile);
             }
-
-            // Build an index of tile coordinates to tiles
-
-            this.tiles = tiles.ToDictionary(tile => tile.Coordinates);
         }
 
         #region Tile Queries
@@ -45,13 +49,76 @@ namespace TribesOfDust.Hex
         #endregion
         #region Manipulation
 
+        /// <summary>
+        /// Adds a <see cref="HexTile"/> to the map.
+        /// </summary>
+        ///
+        /// <exception cref="ArgumentException">
+        /// A tile at the specified coordinates has already been added.
+        /// </exception>
+        ///
+        /// <param name="tile">The tile to add.</param>
+        public void AddTile(HexTile tile)
+        {
+            if (tiles.ContainsKey(tile.Coordinates))
+            {
+                throw new ArgumentException("Duplicate tile coordinates in tile map.", "tile");
+            }
+
+            tiles.Add(tile.Coordinates, tile);
+            AddChild(tile);
+        }
+
+        /// <summary>
+        /// Removes a <see cref="HexTile"/> from the map.
+        /// </summary>
+        ///
+        /// <exception cref="ArgumentException">
+        /// Thrown when the tile that is being removed is not part of the map.
+        /// </exception>
+        ///
+        /// <param name="tile">The tile to remove.</param>
+        public void RemoveTile(HexTile tile)
+        {
+            HexTile? target = GetTileAt(tile.Coordinates);
+
+            // Note that even though we might receive a valid tile, the tile stored at
+            // the coordinates of the provided tile might not be the same, in which
+            // case we need to throw an exception.
+
+            if (target is null || target != tile)
+            {
+                throw new ArgumentException("Trying to remove tile that is not part of the hex map.", "tile");
+            }
+
+            tiles.Remove(target.Coordinates);
+            RemoveChild(target);
+        }
+
+        /// <summary>
+        /// Removes a <see cref="HexTile"/> from the map.
+        /// </summary>
+        ///
+        /// <exception cref="ArgumentException">
+        /// Thrown when there is no tile at the specified coordinates.
+        /// </exception>
+        ///
+        /// <param name="coordinates">The coordinates of the tile to remove.</param>
         public void RemoveTileAt(AxialCoordinate<int> coordinates)
         {
+            HexTile? removed = GetTileAt(coordinates);
+
             // Note that the dictionary won't throw an exception if the coordinates are
             // not registered. Since this is exactly what we want, no additional checks
             // are necessary.
 
+            if (removed is null)
+            {
+                throw new ArgumentException("Trying to remove tile that is not part of the hex map.", "coordinates");
+            }
+
             tiles.Remove(coordinates);
+            RemoveChild(removed);
         }
 
         #endregion
@@ -63,6 +130,6 @@ namespace TribesOfDust.Hex
 
         #endregion
 
-        private readonly Dictionary<AxialCoordinate<int>, HexTile> tiles;
+        private readonly Dictionary<AxialCoordinate<int>, HexTile> tiles = new();
     }
 }
