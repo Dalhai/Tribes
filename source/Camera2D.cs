301003273b728189f43d75df352cd04020102f35
nextom1 <nextom@outlook.com>
2021-10-10 15:35:48 +0200

    "Add camera zoom (#78)"

--- source/Camera2D.cs
+++ source/Camera2D.cs
@@ -3,7 +3,7 @@ using Godot;
 
 namespace TribesOfDust
 {
-    public class Camera2D : Godot.Node2D
+    public class Camera2D : Godot.Camera2D
     {
         public override void _Process(float delta)
         {
@@ -26,6 +26,26 @@ namespace TribesOfDust
             {
                 Position = Position.MoveToward(Position + Vector2.Right, delta * 10000.0f);
             }
+
+            if(Input.IsActionJustReleased("zoom_in"))
+            {
+                Vector2 oldZoom = Zoom;
+                Vector2 newZoom = oldZoom * 0.9f;;
+                Viewport viewport = GetViewport();
+                Position = Position + 0.5f*viewport.Size*(oldZoom-newZoom);
+                Zoom = newZoom;
+            }
+
+
+            if(Input.IsActionJustReleased("zoom_out"))
+            {
+                Vector2 oldZoom = Zoom;
+                Vector2 newZoom = oldZoom * 1.1f;;
+                Viewport viewport = GetViewport();
+                Position = Position + 0.5f*viewport.Size*(oldZoom-newZoom);
+                Zoom = newZoom;
+            }
+            
         }
     }
 }
