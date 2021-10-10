c83bb1c14daa807e40833824de442ea5292595ad
nextom1 <nextom@outlook.com>
2021-10-10 16:35:58 +0200

    Cleanup magic constants (#78)

--- source/Camera2D.cs
+++ source/Camera2D.cs
@@ -5,6 +5,12 @@ namespace TribesOfDust
 {
     public class Camera2D : Godot.Camera2D
     {
+        public const string InputActionZoomIn = "zoom_in";
+        public const string InputActionZoomOut = "zoom_out";
+        public const float ZoomChange = 0.1f;
+        public const float OffsetLengthWeight = 0.15f;
+        public const float OffsetSpeed = 50f;
+
         public override void _Process(float delta)
         {
             if (Input.IsKeyPressed((int)KeyList.Up))
@@ -27,29 +33,34 @@ namespace TribesOfDust
                 Position = Position.MoveToward(Position + Vector2.Right, delta * 10000.0f);
             }
 
-            if(Input.IsActionJustReleased("zoom_in"))
+            if (Input.IsActionJustReleased(InputActionZoomIn))
             {
                 Vector2 oldZoom = Zoom;
-                Vector2 newZoom = oldZoom * 0.9f;
+                Vector2 newZoom = oldZoom * (1.0f - ZoomChange);
+
                 Vector2 offset = GetGlobalMousePosition() - GetCameraScreenCenter();
                 Vector2 offsetNormalized = offset.Normalized();
-                float offsetWeight = (float) Math.Min(offset.Length()*0.15f,50f*Math.Pow(newZoom.x,2f));
-                Viewport viewport = GetViewport();
-                Position = Position + 0.5f*viewport.Size*(oldZoom-newZoom) +  offsetNormalized*offsetWeight;
 
+                float maxOffsetWeight = offset.Length() * OffsetLengthWeight;
+                float currentOffsetWeight = OffsetSpeed * (float)Math.Pow(newZoom.x, 2f);
+                float offsetWeight = (float)Math.Min(maxOffsetWeight, currentOffsetWeight);
+
+                Viewport viewport = GetViewport();
+                Position = Position + 0.5f * viewport.Size * (oldZoom - newZoom) + offsetNormalized * offsetWeight;
                 Zoom = newZoom;
             }
 
 
-            if(Input.IsActionJustReleased("zoom_out"))
+            if (Input.IsActionJustReleased(InputActionZoomOut))
             {
                 Vector2 oldZoom = Zoom;
-                Vector2 newZoom = oldZoom * 1.1f;;
+                Vector2 newZoom = oldZoom * (1.0f + ZoomChange);
+
                 Viewport viewport = GetViewport();
-                Position = Position + 0.5f*viewport.Size*(oldZoom-newZoom);
+                Position = Position + 0.5f * viewport.Size * (oldZoom - newZoom);
                 Zoom = newZoom;
             }
-            
+
         }
     }
 }
