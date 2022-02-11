c36a103c9575a5f9493374ad3f177a07423af4db
Marcel Marti <mmarti@esri.com>
2022-02-11 15:55:33 -0300

    Add getter and setter for water costs (#112)

--- source/data/assets/Movement.cs
+++ source/data/assets/Movement.cs
@@ -1,20 +1,41 @@
 using Godot;
-using TribesOfDust.Hex;
+using TribesOfDust.Data.Currencies;
 
 namespace TribesOfDust.Data.Assets
 {
 	public class Movement : Resource
 	{
 		[Export(PropertyHint.Range, "0,10,1")]
-		int TundraCost = 0;
+		int TundraCost
+		{
+			get => _tundraCost.amount;
+			set => _tundraCost = new Value(Currency.Water, value);
+		}
 
 		[Export(PropertyHint.Range, "0,10,1")]
-		int DunesCost = 0;
+		int DunesCost
+		{
+			get => _dunesCost.amount;
+			set => _dunesCost = new Value(Currency.Water, value);
+		}
 
 		[Export(PropertyHint.Range, "0,10,1")]
-		int MountainsCost = 0;
+		int MountainsCost
+		{
+			get => _mountainsCost.amount;
+			set => _mountainsCost = new Value(Currency.Water, value);
+		}
 
 		[Export(PropertyHint.Range, "0,10,1")]
-		int CanyonsCost = 0;
+		int CanyonsCost
+		{
+			get => _canyonsCost.amount;
+			set => _canyonsCost = new Value(Currency.Water, value);
+		}
+
+		private Value _tundraCost = new Value(Currency.Water, 1);
+		private Value _dunesCost = new Value(Currency.Water, 1);
+		private Value _mountainsCost = new Value(Currency.Water, 1);
+		private Value _canyonsCost = new Value(Currency.Water, 1);
 	}
 }
