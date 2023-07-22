86c7bdd782599f2aeb440fc72472de3ef2899b7f
Dalhai <mm@m-co.ch>
2023-07-22 18:13:43 +0200

    Add buildings, building class resources and repositories

---
+++ Script/Core/Entities/Building.cs
@@ -0,0 +1,44 @@
+﻿using Godot;
+using TribesOfDust.Hex;
+
+namespace TribesOfDust.Core.Entities;
+
+public abstract class Building : IEntity
+{
+    #region Constructors
+    
+    protected Building(AxialCoordinate coordinates, BuildingClass @class)
+    {
+        Coordinates = coordinates;
+        
+        Identity = Identities.GetNextIdentity();
+        
+        Sprite = new();
+        Sprite.Texture = @class.Texture2D;
+        Sprite.Modulate = Colors.Beige;
+        Sprite.ZIndex = 10;
+        
+        // Initialize and reduce scale so the building sprite fits
+
+        float widthScaleToExpected = @class.Texture2D != null ? HexConstants.DefaultWidth / @class.Texture2D.GetWidth() : 1.0f;
+        float heightScaleToExpected = @class.Texture2D != null ? HexConstants.DefaultHeight / @class.Texture2D.GetHeight() : 1.0f;
+        
+        Sprite.Scale = new Vector2(widthScaleToExpected, heightScaleToExpected);
+        Sprite.Scale *= 0.5f;
+        
+        // Position unit according to specified coordinates
+
+        Sprite.Centered = true;
+        Sprite.Position = HexConversions.HexToUnit(coordinates) * HexConstants.DefaultSize;
+    }
+    
+    #endregion
+    #region Queries
+    
+    public ulong Identity { get; }
+    
+    public AxialCoordinate Coordinates { get; }
+    public Sprite2D Sprite { get; }
+    
+    #endregion
+}
---
+++ Script/Core/Entities/BuildingClassRepository.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+
+using Godot;
+using TribesOfDust.Utils;
+
+namespace TribesOfDust.Core.Entities;
+
+public class BuildingClassRepository : Repository<string, BuildingClass>
+{
+    /// <summary>
+    /// The default resource path used for unit class assets.
+    /// </summary>
+    private static readonly string DefaultPath = "res://Assets/Buildings";
+    
+    /// <summary>
+    /// Loads the default assets of the repository.
+    /// </summary>
+    ///
+    /// <remarks>
+    /// A repository should provide a default way to initialize.
+    /// This will however not be automatically called, but must be executed by users when they
+    /// are ready to load - and unload - assets.
+    /// </remarks>
+    ///
+    /// <returns>A list of loaded assets.</returns>
+    protected override List<BuildingClass> LoadAll() => LoadAll(DefaultPath);
+
+    /// <summary>
+    /// Try to load a single asset from the specified resource path.
+    /// </summary>
+    ///
+    /// <param name="resourcePath">The path to the resource.</param>
+    /// <param name="asset">The asset to be initialized, if found.</param>
+    ///
+    /// <returns>True, if the asset could be loaded, false otherwise.</returns>
+    protected override bool TryLoad(string resourcePath, out BuildingClass? asset)
+    {
+        GD.Print($"Loading Building Class: {resourcePath}");
+        var resource = GD.Load(resourcePath);
+        asset = resource as BuildingClass;
+
+        if (asset is { Texture2D: not null })
+        {
+            return true;
+        }
+
+        return false;
+    }
+}
