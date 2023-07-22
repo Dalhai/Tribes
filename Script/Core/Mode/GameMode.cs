72a9476d3d0fbb15e64d2717421b630006decd8f
Dalhai <mm@m-co.ch>
2023-07-22 18:44:06 +0200

    Spawn units around initial, fixed camps

--- Script/Core/Mode/GameMode.cs
+++ Script/Core/Mode/GameMode.cs
@@ -1,6 +1,8 @@
 using System;
 using Godot;
+using TribesOfDust.Core.Controllers;
 using TribesOfDust.Core.Entities;
+using TribesOfDust.Core.Entities.Buildings;
 using TribesOfDust.Hex;
 
 namespace TribesOfDust.Core.Modes;
@@ -37,22 +39,46 @@ public partial class GameMode : Node2D, IUnique<GameMode>
         foreach (var tile in _context.Map.Hexes)
             AddChild(tile.Value.Sprite);
         
-        // Register units
-        var @class = _context.Repos.Units.GetAsset();
-        var unit1 = new Unit(new(-2, -4), @class);
-        var unit2 = new Unit(new( 2,  1), @class);
-        var unit3 = new Unit(new( 1, -2), @class);
-        
-        _context.Map.Units.Add(unit1, unit1.Coordinates);
-        _context.Map.Units.Add(unit2, unit2.Coordinates);
-        _context.Map.Units.Add(unit3, unit3.Coordinates);
-        
-        AddChild(unit1.Sprite);
-        AddChild(unit2.Sprite);
-        AddChild(unit3.Sprite);
-        
         // Register buildings
+        var buildingClass = _context.Repos.Buildings.GetAsset();
+        var camp1 = new Camp(new(-2, -3), buildingClass, _player1);
+        var camp2 = new Camp(new( 5,  4), buildingClass, _player2);
         
+        AddChild(camp1.Sprite);
+        AddChild(camp2.Sprite);
+        
+        // Register units
+        var unitClass = _context.Repos.Units.GetAsset();
+        
+        if (camp1.Owner != null)
+        {
+            var unit1 = new Unit(camp1.Coordinates.N, unitClass, camp1.Owner);
+            var unit2 = new Unit(camp1.Coordinates.NE, unitClass, camp1.Owner);
+            var unit3 = new Unit(camp1.Coordinates.SE, unitClass, camp1.Owner);
+        
+            _context.Map.Units.Add(unit1, unit1.Coordinates);
+            _context.Map.Units.Add(unit2, unit2.Coordinates);
+            _context.Map.Units.Add(unit3, unit3.Coordinates);
+        
+            AddChild(unit1.Sprite);
+            AddChild(unit2.Sprite);
+            AddChild(unit3.Sprite);
+        }
+        
+        if (camp2.Owner != null)
+        {
+            var unit1 = new Unit(camp2.Coordinates.N, unitClass, camp2.Owner);
+            var unit2 = new Unit(camp2.Coordinates.NE, unitClass, camp2.Owner);
+            var unit3 = new Unit(camp2.Coordinates.SE, unitClass, camp2.Owner);
+        
+            _context.Map.Units.Add(unit1, unit1.Coordinates);
+            _context.Map.Units.Add(unit2, unit2.Coordinates);
+            _context.Map.Units.Add(unit3, unit3.Coordinates);
+        
+            AddChild(unit1.Sprite);
+            AddChild(unit2.Sprite);
+            AddChild(unit3.Sprite);
+        }
 
         base._Ready();
     }
@@ -70,4 +96,6 @@ public partial class GameMode : Node2D, IUnique<GameMode>
     }
 
     private MapContext _context = null!;
+    private Player _player1 = new("Player 1", Colors.Red);
+    private Player _player2 = new("Player 2", Colors.Blue);
 }
