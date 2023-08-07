b9368e71030c0a30f3fb165002b6b893a93db97b
Dalhai <mm@m-co.ch>
2023-08-07 17:18:23 +0200

    Add new configuration and identity interfaces

---
+++ Script/Core/IIdentifiable.cs
@@ -0,0 +1,18 @@
+﻿namespace TribesOfDust.Core;
+
+public interface IIdentifiable
+{
+    /// <summary>
+    /// The unique identity of the entity.
+    /// </summary>
+    ulong Identity { get; }
+}
+
+public static class Identities
+{
+    /// <summary>
+    /// Gets the next globally unique identity.
+    /// </summary>
+    public static ulong GetNextIdentity() => _lastIdentity++;
+    private static ulong _lastIdentity;
+}
