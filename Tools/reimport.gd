115039b8f5807b852acaa0c4f9b8311b7cae07ba
Dalhai <mm@m-co.ch>
2025-07-04 19:40:52 +0200

    Fix invalid UIDs for all tres assets (#120)

---
+++ Tools/reimport.gd
@@ -0,0 +1,22 @@
+# Save this as, say, `res://tools/fix_uids.gd`, then right-click ▶ Run
+@tool
+extends EditorScript
+
+var files: Array[String] = []
+
+func _run() -> void:
+	files.clear()
+	_collect_files("res://")
+	for file in files:
+		print("⟳ Fixing UID in:", file)
+		var res = ResourceLoader.load(file)
+		ResourceSaver.save(res)
+
+func _collect_files(dir_path: String) -> void:
+	var dir = DirAccess.open(dir_path)
+	for file_name in dir.get_files():
+		var ext = file_name.get_extension().to_lower()
+		if ext in ["tscn", "tres", "material"]:
+			files.append(dir_path + "/" + file_name)
+	for child in dir.get_directories():
+		_collect_files(dir_path + "/" + child)
